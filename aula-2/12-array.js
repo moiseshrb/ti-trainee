59068184dec32987b5e76b1d5fcbc2c5441985eb
moiseshrb <henriquebrbx@gmail.com>
2024-04-22 13:27:25 -0300

    v2.0

---
+++ aula-2/12-array.js
@@ -0,0 +1,49 @@
+// Array de números
+const numbers = [1, 2, 3, 4, 5];
+// const numbers2 = numbers.pop()
+// console.log(numbers2)
+// console.log(numbers)
+
+// Array de strings
+const fruits = ['maçã', 'banana', 'laranja', 'uva'];
+// const fruitsNinja = fruits.shift()
+// const fruitsYellow = fruits.unshift('Cajá', 'Melão')
+// console.log(fruits)
+// console.log(fruitsNinja)
+
+// Array de objetos
+const persons = [
+  { nome: 'João', idade: 30 },
+  { nome: 'Maria', idade: 25 },
+  { nome: 'Pedro', idade: 35 }
+];
+
+// persons.push({
+//   nome: 'Jonmar', idade: 99
+// })
+
+// Array misto
+const mixedArray = [1, 'dois', true, { chave: 'valor' }];
+
+// Impressão dos arrays
+// console.log('Array de números:', numbers);
+// console.log('Array de frutas:', fruits);
+// console.log('Array de pessoas:', persons);
+// console.log('Array misto:', mixedArray);
+
+const array = [1, 2, 3, 4, 5];
+array.splice(2, 1, 'a', 'b'); // indice, números a serem removidos, ...elememento a serem adicionados*
+console.log(array); // Saída: [1, 2, 'a', 'b', 4, 5]
+
+const array9 = [1, 2, 3, 4, 5];
+const newArray = array9.slice(1, 3); // inicio, final*  e aceitam parâmetros negativos
+console.log(newArray); // Saída: [2, 3]
+
+const array99 = [1, 2, 3];
+array99.forEach(item => console.log(item));
+// Saída:
+// 1
+// 2
+// 3
+
+
--- aula-2/12-estrutura-dados.md
+++ aula-2/12-estrutura-dados.md
@@ -1,14 +1,30 @@
-### Lista ligada
+## Lista ligada
 Coleção ordernada de valores onde cada elemento aponta para o próximo 
 
 (Diferença de Js pra outras linguagens)
 
-### Array
+## Array
 Coleção de elementos que podem ser identificados por um índice
 
 (Métodos: push, pop, unshift, shift, length e etc...)
 
-### Fila (Queue)
+## Fila (Queue)
 Coleção de elementos onde o primeiro elemento 
 ![Exemplo de fila](fila-queue.png)
+
+## Pilha (Stack)
+Coleção de elementos onde o primeiro elemento a ser retirado deve ser o último elemento adicionado. Last-in / First-out
+![Exemplo de pilha](pilha-queue.png)
+
+## Árvore (Tree)
+Conjuntos de elementos chamados de nós (ou nodes) organizados em estrutura hierárquica (não sequencial), ou seja, podendo estar "abaixo" ou "acima" de outros nós.
+![Exemplo de árvore](arvore-tree.png)
+
+Em Javascript:
+  Lista == Array
+  Dicionário == Objeto
+
+
+Valor != Referência
+
 
